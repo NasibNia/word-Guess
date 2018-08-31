7fb7754293586aecf10970fcad6fcaaeeb6afb8f
NasibNia <nasibnia@gmail.com>
2018-08-30 18:42:18 -0700

    header added

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ var word = require('./word.js');
 // Bringing in the inquirer
 var inquirer = require ('inquirer');
 //Bringing in the chalk to change the color in terminal
-var chalk = require('chalk');
- 
+var chalk = require('chalk'); 
+
+var CFonts = require('cfonts');
+
+//display the game header 
+cFontDisplay('FRUITY | HANGMAN','block');
+cFontDisplay("======================", 'chrome');
+cFontDisplay("Let's get to some|fruits!",'chrome');
+cFontDisplay("======================", 'chrome');
+
+
 
 // The list of words for the computer guess
 var list  = ["chry" , "ban" , "lim"];
@@ -30,9 +39,9 @@ function randomWord (){
   var indx = Math.floor(Math.random()*list.length);
   //select the random word from list
   var randWord = list[indx];
-  console.log("random word is " + randWord);
+//   console.log("random word is " + randWord);
 
-  console.log("track object " , trackObj);
+//   console.log("track object " , trackObj);
   
   //check to see if random word is already selected or not
   if (randWord in trackObj){
@@ -50,7 +59,7 @@ function randomWord (){
       }     
   } // if the random word has not already selected, add it to the trackObj for the future reference and return it.
   else {
-    console.log("adding it to the tracker");
+    // console.log("adding it to the tracker");
      trackObj[randWord] = true;
      return randWord;
   }  
@@ -62,9 +71,9 @@ function randomWord (){
 // };
 // tst_randomWord();
 //============================= Selecting Random Word =====================
+// Convert the gif file into text frames
 
 var guessCount = 0;
-
 var tmp = randomWord();
 var newWord = new word(tmp);
 //call makeLetters function on newWord to create an array of Letter objects
@@ -92,6 +101,7 @@ var previousState = [];
 for (var i  = 0 ; i< tmp.length ; i++){
     previousState.push(false);
 }
+
 
 
 function playGame (){
@@ -147,8 +157,7 @@ function playGame (){
             playGame();
         }
         else{
-            console.log("\n");
-            console.log(chalk.blue("You got it right! Next word!"));
+            console.log(chalk.bgCyan.bold("You got it right! Next word!"));
             console.log("\n-------------------------\n");
             tmp = randomWord();
             newWord = new word(tmp);
@@ -174,7 +183,19 @@ function areSameArr (a,b){
     return true;
 }
 
-
+// function to show str in fun formats provided by CFont.
+function cFontDisplay (str , format){
+    CFonts.say(str, {
+        font: format,              // define the font face
+        align: 'center',              // define text alignment
+        colors: ['magenta','blue','cyan'],         // define all colors
+        background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+        letterSpacing: 0,           // define letter spacing
+        lineHeight: 0,              // define the line height
+        space: false,                // define if the output text should have empty lines on top and on the bottom
+        maxLength: '0',             // define how many character can be on one line
+    });
+}
 
 
 
